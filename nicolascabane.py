9559468979b2eda7a10945a29fd1eff8f72921ee
ncabane <ncabane@gmail.com>
2022-09-04 20:58:10 +0200

    Finish 3rd test

--- nicolascabane.py
+++ nicolascabane.py
@@ -10,51 +10,81 @@ driver.get('https://nicolascabane.com')
 clickMenu = driver.find_element(By.CSS_SELECTOR, '.navbar-toggler-icon')
 clickMenu.click();
 
-# Waits 3 seconds
-time.sleep(3)
+# Waits 2 seconds
+time.sleep(2)
 
 # Clicks CV
 clickCv = driver.find_element(By.LINK_TEXT, 'CV')
 clickCv.click();
 
-# Waits 3 seconds
-time.sleep(3)
+# Waits 2 seconds
+time.sleep(2)
 
 # Clicks hamburger menu
 clickMenu = driver.find_element(By.CSS_SELECTOR, '.navbar-toggler-icon')
 clickMenu.click();
 
-# Waits 3 seconds
-time.sleep(3)
+# Waits 2 seconds
+time.sleep(2)
 
 # Clicks SERVICIOS
 clickServ = driver.find_element(By.LINK_TEXT, 'SERVICIOS')
 clickServ.click();
 
-# Waits 3 seconds
-time.sleep(3)
+# Waits 2 seconds
+time.sleep(2)
 
 # Clicks hamburger menu
 clickMenu = driver.find_element(By.CSS_SELECTOR, '.navbar-toggler-icon')
 clickMenu.click();
 
-# Waits 3 seconds
-time.sleep(3)
+# Waits 2 seconds
+time.sleep(2)
 
 # Clicks SOBRE MI
 clickServ = driver.find_element(By.LINK_TEXT, 'SOBRE MI')
 clickServ.click();
 
-# Waits 3 seconds
-time.sleep(3)
+# Waits 2 seconds
+time.sleep(2)
 
 # Clicks hamburger menu
 clickMenu = driver.find_element(By.CSS_SELECTOR, '.navbar-toggler-icon')
 clickMenu.click();
 
-# Waits 3 seconds
-time.sleep(3)
+# Waits 2 seconds
+time.sleep(2)
 
 # Clicks CONTACTO
 clickServ = driver.find_element(By.LINK_TEXT, 'CONTACTO')
 clickServ.click();
+
+# Goes back to home page
+time.sleep(2)
+backHome = driver.find_element(By.CSS_SELECTOR, '.site-title')
+backHome.click();
+
+# Fills form name
+time.sleep(2)
+formName = driver.find_element(By.ID, 'wpforms-52-field_0')
+formName.send_keys("Juana")
+
+# Fills form last name
+formLast = driver.find_element(By.ID, 'wpforms-52-field_0-last')
+formLast.send_keys("Manso")
+
+# Fills email field
+formEmail = driver.find_element(By.ID, 'wpforms-52-field_1')
+formEmail.send_keys("juanamanso@juanamanso.com")
+
+# Check one checkbox
+checkThat = driver.find_element(By.ID, 'wpforms-52-field_4_1')
+checkThat.click();
+
+# Press send button
+sendBtn = driver.find_element(By.ID, 'wpforms-submit-52')
+sendBtn.click();
+time.sleep(4)
+
+# Closes the browser
+driver.close()
