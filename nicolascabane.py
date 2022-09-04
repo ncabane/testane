ce1428042ff327a1617c5cb99d9406d3ae4d2afb
ncabane <ncabane@gmail.com>
2022-09-04 10:18:08 +0200

    almost completes third test

--- nicolascabane.py
+++ nicolascabane.py
@@ -10,17 +10,51 @@ driver.get('https://nicolascabane.com')
 clickMenu = driver.find_element(By.CSS_SELECTOR, '.navbar-toggler-icon')
 clickMenu.click();
 
+# Waits 3 seconds
+time.sleep(3)
+
 # Clicks CV
 clickCv = driver.find_element(By.LINK_TEXT, 'CV')
 clickCv.click();
 
-# Clicks SERVICIOS
+# Waits 3 seconds
+time.sleep(3)
+
+# Clicks hamburger menu
+clickMenu = driver.find_element(By.CSS_SELECTOR, '.navbar-toggler-icon')
 clickMenu.click();
+
+# Waits 3 seconds
+time.sleep(3)
+
+# Clicks SERVICIOS
 clickServ = driver.find_element(By.LINK_TEXT, 'SERVICIOS')
 clickServ.click();
 
+# Waits 3 seconds
+time.sleep(3)
 
+# Clicks hamburger menu
+clickMenu = driver.find_element(By.CSS_SELECTOR, '.navbar-toggler-icon')
+clickMenu.click();
 
+# Waits 3 seconds
+time.sleep(3)
 
+# Clicks SOBRE MI
+clickServ = driver.find_element(By.LINK_TEXT, 'SOBRE MI')
+clickServ.click();
 
+# Waits 3 seconds
+time.sleep(3)
 
+# Clicks hamburger menu
+clickMenu = driver.find_element(By.CSS_SELECTOR, '.navbar-toggler-icon')
+clickMenu.click();
+
+# Waits 3 seconds
+time.sleep(3)
+
+# Clicks CONTACTO
+clickServ = driver.find_element(By.LINK_TEXT, 'CONTACTO')
+clickServ.click();
