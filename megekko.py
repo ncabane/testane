ef461c21340c746a059ca54a411ea721aa60a6c2
ncabane <ncabane@gmail.com>
2022-09-12 21:15:16 +0200

    New Megekko Test

--- megekko.py
+++ megekko.py
@@ -1,6 +1,8 @@
+from textwrap import fill
 from webbrowser import Chrome
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time 
 
 driver = webdriver.Chrome()
@@ -16,4 +18,26 @@ time.sleep(2)
 catOpen = driver.find_element(By.ID, 'searchFieldInputField')
 catOpen.click();
 catOpen.send_keys("Nvidia")
+catOpen.send_keys(Keys.ENTER);
+
+# Places an item in the shop cart
+time.sleep(2)
+addCart = driver.find_element(By.CLASS_NAME, 'shopbutton')
+addCart.click();
+
+# Fills email field
+time.sleep(2)
+fillEmail = driver.find_element(By.ID, 'email')
+fillEmail.click();
+fillEmail.send_keys("testane@testane.com")
+
+# Selects country Nederland
+time.sleep(2)
+seLand = driver.find_element(By.ID, 'land')
+seLand.click();
+time.sleep(2)
+seLand.click();
+
+
+
 
