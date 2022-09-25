c1a4ca700c1b359e9a8c53c6cad6dd564d2df2eb
ncabane <ncabane@gmail.com>
2022-09-25 20:28:20 +0200

    Extends e2e full test

--- e2epractice.py
+++ e2epractice.py
@@ -7,3 +7,13 @@ import time
 # Opens Linkedin homepage without login in
 driver = webdriver.Chrome()
 driver.get('http://automationpractice.com/index.php/')
+
+# Clicks on Women category
+time.sleep(2)
+clkWomen = driver.find_element(By.LINK_TEXT, 'WOMEN')
+clkWomen.click();
+
+# Clicks on Tops subcategory
+time.sleep(2)
+clktTops = driver.find_element(By.LINK_TEXT, 'TOPS')
+clktTops.click();
